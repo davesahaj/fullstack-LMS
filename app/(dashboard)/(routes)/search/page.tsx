9c7e5e5921aa5956ea7328f7cf0576e96f46fc26
Dave <sahaj.dave@yappes.com>
2023-11-21 00:24:23 +0530

    routes + sidebar

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function SearchPage() {
+  return <div>this is a search page</div>;
+}
---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+const Logo = (props: Props) => {
+  return <Image height={130} width={130} alt="logo" src="/logo.svg" />;
+};
+
+export default Logo;
