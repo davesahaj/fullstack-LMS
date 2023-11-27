e15beeac6a26b64f87ddd6dbd663e02f041f5503
Dave <sahajdavework@pm.me>
2023-11-27 12:58:44 +0530

    added axios, react hook form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const CoursesPage = (props: Props) => {
-  return <div>CoursesPage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
