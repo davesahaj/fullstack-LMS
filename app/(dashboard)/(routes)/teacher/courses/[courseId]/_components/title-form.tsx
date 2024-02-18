09ee1eecbf0ea5b452335234d38701d6ec451ceb
Dave <sahajdavework@pm.me>
2024-02-18 18:28:29 +0530

    added form submit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -58,6 +58,38 @@ export const TitleForm = ({ courseId, initialData }: TitleFormProps) => {
           )}
         </Button>
       </div>
+      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Web Development"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
     </div>
   );
 };
